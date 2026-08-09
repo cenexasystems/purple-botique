2fca4fa141b04658a81bb563d6ad5b28b76afdd6
sanjanasenthilkumar15 <sanjanasenthilkumar15@gmail.com>
2026-08-09 19:15:20 +0530

    fix: remove duplicated footer from pos view

--- purple-boutique-billing/src/pages/Pos.tsx
+++ purple-boutique-billing/src/pages/Pos.tsx
@@ -1443,9 +1443,7 @@ export default function Pos(props: PosProps = {}) {
         />
       )}
 
-      <div className="mt-8 pb-4 text-center text-xs font-bold text-[#858C83]">
-        Powered by Cenexa Systems &copy; 2026
-      </div>
+
     </div>
   )
 }
